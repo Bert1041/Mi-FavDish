c74d11992499d1f64864c056e77b4554a4549dbf
Your Name <ishokarobertt@gmail.com>
2021-09-13 16:54:10 +0100

    Splash Screen Animation and App Icon

--- app/src/main/java/com/ripper/mifavdish/SplashActivity.kt
+++ app/src/main/java/com/ripper/mifavdish/SplashActivity.kt
@@ -1,13 +1,54 @@
 package com.ripper.mifavdish
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.annotation.RequiresApi
 import com.ripper.mifavdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val splashBinding : ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
+
+        //hiding status bar
+        if(Build.VERSION.SDK_INT >+ Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        //splashBinding.tvAppName.text = "hello world"
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        splashBinding.tvAppName.animation = splashAnimation
+
+
+
+        splashAnimation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationStart(p0: Animation?) {
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                  startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+            }
+
+        })
     }
 }
