f016b96a6d4c76bac3dc713a3567c1a0c81646ed
Your Name <ishokarobertt@gmail.com>
2021-09-25 02:39:47 +0100

    Update Dish Activity and intuit(scalable size unit)

--- app/src/main/java/com/ripper/mifavdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/ripper/mifavdish/view/activities/AddUpdateDishActivity.kt
@@ -3,10 +3,40 @@ package com.ripper.mifavdish.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ripper.mifavdish.R
+import com.ripper.mifavdish.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupActionBar()
     }
+
+
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+//        if (mFavDishDetails != null && mFavDishDetails!!.id != 0) {
+//            supportActionBar?.let {
+//                it.title = resources.getString(R.string.title_edit_dish)
+//            }
+//        } else {
+//            supportActionBar?.let {
+//                it.title = resources.getString(R.string.title_add_dish)
+//            }
+//        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
 }
+
+
+
